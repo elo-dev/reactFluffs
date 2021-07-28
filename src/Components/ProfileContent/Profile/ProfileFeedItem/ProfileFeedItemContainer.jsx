48f0f831541f597c5eb3fd80f1f39576ae5a3188
Dima Frolov <dimasiys25@gmail.com>
2021-07-29 00:22:11 +0300

    Preloader

--- src/Components/ProfileContent/Profile/ProfileFeedItem/ProfileFeedItemContainer.jsx
+++ src/Components/ProfileContent/Profile/ProfileFeedItem/ProfileFeedItemContainer.jsx
@@ -1,13 +1,52 @@
+import React from 'react'
+import axios from 'axios'
 import { connect } from 'react-redux'
-import { setCurrentPageActionCreator, setUsersActionCreator, setTotalUsersCountActionCreator, likeActionCreator, unLikeActionCreator, bookmarkActionCreator, unBookmarkActionCreator } from '../../../../redux/profileItemsReducer'
+import { setCurrentPageActionCreator, setUsersActionCreator, setTotalUsersCountActionCreator, likeActionCreator, unLikeActionCreator, bookmarkActionCreator, unBookmarkActionCreator, toggleIsFetching } from '../../../../redux/profileItemsReducer'
 import ProfileFeedItem from './ProfileFeedItem'
+import Preloader from '../../../common/Preloader/Preloader'
+
+class ProfileFeedItemContainer extends React.Component{
+
+    componentDidMount(){
+        this.props.toggleIsFetching(true)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users/?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.toggleIsFetching(false)
+            this.props.setUsers(response.data.items)
+            this.props.setTotalUsersCount(response.data.totalCount)
+        })
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        this.props.toggleIsFetching(true)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users/?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.toggleIsFetching(false)
+            this.props.setUsers(response.data.items)
+        })
+    }
+
+    render(){
+        return(
+            <>
+            {this.props.isFetching ? <Preloader /> : null}
+            <ProfileFeedItem posts={this.props.posts}
+                             pageSize={this.props.pageSize}
+                             totalUsersCount={this.props.totalUsersCount}
+                             currentPage={this.props.currentPage}
+                             onPageChanged={this.onPageChanged}
+                             toggleModal={this.props.onClick} />
+            </>
+        )
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
         posts: state.profileItems.posts,
         pageSize: state.profileItems.pageSize,
         totalUsersCount: state.profileItems.totalUsersCount,
-        currentPage: state.profileItems.currentPage
+        currentPage: state.profileItems.currentPage,
+        isFetching: state.profileItems.isFetching
     }
 }
 
@@ -33,8 +72,11 @@ let mapDispatchToProps = (dispatch) => {
         },
         setTotalUsersCount: (totalCount) => {
             dispatch(setTotalUsersCountActionCreator(totalCount))
+        },
+        toggleIsFetching: (isFetching) => {
+            dispatch(toggleIsFetching(isFetching))
         }
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProfileFeedItem)
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileFeedItemContainer)
--- src/redux/profileItemsReducer.js
+++ src/redux/profileItemsReducer.js
@@ -5,13 +5,15 @@ const BOOKMARK = 'BOOKMARK'
 const UNBOOKMARK = 'UNBOOKMARK'
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
 const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
+const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING'
 
 let initialState = {
   posts: [],
   pageSize: 5,
   totalUsersCount: 0,
   currentPage: 1,
-  countLike: 520
+  countLike: 520,
+  isFetching: true
 }
 
 const UsersReducer = (state = initialState, action) => {
@@ -63,6 +65,8 @@ const UsersReducer = (state = initialState, action) => {
         return {...state, currentPage: action.currentPage}
       case SET_TOTAL_USERS_COUNT: 
         return {...state, totalUsersCount: action.count}
+      case TOGGLE_IS_FETCHING:
+        return {...state, isFetching: action.isFetching}
       default: 
         return state
     }
@@ -114,6 +118,13 @@ export const setTotalUsersCountActionCreator = (totalUsers) => {
   return {
     type: SET_TOTAL_USERS_COUNT,
     count: totalUsers
+  }
+}
+
+export const toggleIsFetching = (isFetching) => {
+  return {
+    type: TOGGLE_IS_FETCHING,
+    isFetching
   }
 }
 
